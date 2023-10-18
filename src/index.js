d66fa0dd7a7ad0b1033598dccc1d6672b1f7bfbc
Gina <gina.nikol@gmail.com>
2023-10-18 21:31:42 +0300

    initial commit

---
+++ src/index.js
@@ -0,0 +1,4 @@
+const ebsiResolver = require('@cef-ebsi/ebsi-did-resolver');
+const verifiableCredential = require('@cef-ebsi/verifiable-credential');
+const verifiablePresentation = require('@cef-ebsi/verifiable-presentation');
+const keyDIDResolver = require('@cef-ebsi/key-did-resolver');
