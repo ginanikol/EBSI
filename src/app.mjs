8a84de705de03dafe3cf689baac5632aa6377680
Gina Nikolouzou <georgia.nikolouzou@arhs-dev-hellas.com>
2023-10-23 10:54:25 +0300

    Updated

---
+++ src/app.mjs
@@ -0,0 +1,52 @@
+// Import required modules
+import express from 'express';
+import { getResolver } from "@cef-ebsi/key-did-resolver";
+import { util } from "@cef-ebsi/key-did-resolver";
+import {Resolver} from "did-resolver";
+import { randomBytes } from 'crypto';
+import cors from 'cors';
+
+// Create an Express app
+const app = express();
+
+app.use(express.json());
+
+app.use(
+  cors({
+    origin: 'http://localhost:8081',
+    methods: ['GET', 'POST'],
+    optionsSuccessStatus: 204,
+  })
+);
+
+// Define a route for generating a new DID
+app.get('/generate-did', (req, res) => {
+  try {
+    const subjectIdentifierBytes = randomBytes(16);
+    const did = util.createDid(subjectIdentifierBytes);
+    res.json({ did });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: 'An error occurred while generating a new DID.' });
+  }
+});
+
+//////////////////////////////////////////////////////////////////////////////////
+
+app.post('/create-did-from-jwk', (req, res) => {
+  try {
+      const jwk = req.body;
+      const did = util.createDid(jwk);
+      res.json({ did });
+  } catch (error) {
+      console.error(error);
+      res.status(500).json({ error: 'An error occurred while creating a DID from the provided JWK.' });
+  }
+});
+
+//////////////////////////////////////////////////////////////////////////////////
+
+const port = 8000;
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
